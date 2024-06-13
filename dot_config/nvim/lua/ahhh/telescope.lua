e815a636723813feb42556671a94b2dd63f12098
Sam Baumohl <sam.baumohl@thomsonreuters.com>
2024-06-12 21:22:28 -0500

    push telescope

---
+++ dot_config/nvim/lua/ahhh/telescope.lua
@@ -0,0 +1,6 @@
+require('telescope').setup()
+
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
