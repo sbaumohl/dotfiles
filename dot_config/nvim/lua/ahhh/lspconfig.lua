bac321cb1bdbff52c52abbac3cef929504e5afd2
sbaumohl <28550422+sbaumohl@users.noreply.github.com>
2025-07-06 18:01:06 -0500

    go-to definition keybind

--- dot_config/nvim/lua/ahhh/lspconfig.lua
+++ dot_config/nvim/lua/ahhh/lspconfig.lua
@@ -67,6 +67,9 @@ vim.lsp.config("eslint", {
 
 vim.lsp.enable({ "astro", "pyright", "eslint", "dockerls", "clangd" })
 
+vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+
 -- require("typescript-tools").setup({})
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
