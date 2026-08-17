a5c8d052407bdfa2e2d5300d355e1117b2062273
sbaumohl <28550422+sbaumohl@users.noreply.github.com>
2026-08-17 00:06:09 -0500

    small config update

--- dot-config/wezterm/wezterm.lua
+++ dot-config/wezterm/wezterm.lua
@@ -1,8 +1,10 @@
 local wezterm = require 'wezterm'
-local config = wezterm.config_builder() 
+local config = wezterm.config_builder()
 
 config.font = wezterm.font_with_fallback({
 	'lilex', 'JetBrains Mono', 'Symbols Nerd Font'
 })
+-- config.enable_wayland = false;
+
 
 return config
